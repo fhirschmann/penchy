46358834afc84fedfc5decb2c245c74c3c0aa64c
Fabian Hirschmann <fabian@hirschm.net>
2011-11-21 15:28:55 +0100

    [server] restructured server code

--- server.py
+++ server.py
@@ -10,20 +10,57 @@ from config import *
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger("server")
 
-if __name__ == "__main__":
-    for node in NODES:
-        log.info("Connecting to Node %s" % str(node))
-        transport = paramiko.Transport((node[0], node[1]))
-        transport.connect(username=node[2],
-                pkey = paramiko.RSAKey.from_private_key_file(ID_RSA))
-        sftp = paramiko.SFTPClient.from_transport(transport)
-        
-        try:
-            sftp.mkdir(node[3])
-        except IOError:
-            log.debug("Directory %s exists already" % node[3])
+class Node(object):
+    """
+    This class represents a node (=a machine on which the benchmark
+    will be run on).
+    """
+    def __init__(self, node, pkey):
+        """Initialize the node
 
+        :param node: tuple of (hostname, port, username, remote path)
+        :type pkey: paramiko.RSAKey
+        """
+
+        self.host, self.port, self.username, self.path = node
+        self.pkey = pkey
+        self.transport = paramiko.Transport((self.host, self.port))
+        self.sftp = None
+        self.ssh = None
+
+    def connect(self):
+        """Connect to the node"""
+
+        log.info("Connecting to node %s" % self.host)
+        self.transport.connect(username = self.username, pkey = self.pkey)
+        self.sftp = paramiko.SFTPClient.from_transport(self.transport)
+        self.ssh = self.transport.open_session()
+
+        # Create the directory we will be uploading to
+        try:
+            self.sftp.mkdir(self.path)
+        except IOError:
+            pass
+
+    def put(self, filename):
+        """Upload a file to the node
+
+        :param filename: string
+        """
+
+        location = self.path + os.path.sep + os.path.basename(filename)
+        log.info("Copying file %s to %s" % (filename, location))
+        self.sftp.put(sys.path[0] + os.path.sep + filename, location)
+
+
+if __name__ == "__main__":
+    pkey = paramiko.RSAKey.from_private_key_file(ID_RSA)
+
+    nodes = []
+    for node in NODES:
+        nodes.append(Node(node, pkey))
+
+    for node in nodes:
+        node.connect()
         for f in FILES:
-            rloc = node[3] + os.path.sep + os.path.basename(f)
-            log.info("Copying file %s to %s" % (f, rloc))
-            sftp.put(sys.path[0] + '/' + f, rloc)
+            node.put(f)
