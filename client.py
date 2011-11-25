3e003a45210e5dcc7221611575fef4f5fcad02c0
Michael Markert <markert.michael@googlemail.com>
2011-11-25 11:48:53 +0100

    [client] Make send function a parameter for easier testing.
    Signed-off-by: Michael Markert <markert.michael@googlemail.com>

--- client.py
+++ client.py
@@ -6,7 +6,16 @@ import subprocess
 
 import analyzers
 
-def run_benchmark(jvm, benchmark, options=[]):
+def send_data(filtered_output):
+    """
+    Send filtered benchmark output to server.
+
+    :param filtered_output: relevant benchmark output
+    """
+    # XXX: testing stub
+    print filtered_output
+
+def run_benchmark(jvm, benchmark, options=[], send_function=send_data):
     """
     Run :param:`benchmark` on :param:`jvm`.
 
@@ -20,17 +29,13 @@ def run_benchmark(jvm, benchmark, options=[]):
     _, out = proc.communicate()
     analyzer = analyzers.get_analyzer(benchmark)
     filtered = analyzer(out)
-    send_data(filtered)
-
-def send_data(filtered_output):
-    """
-    Send filtered benchmark output to server.
-
-    :param filtered_output: relevant benchmark output
-    """
-    # XXX: testing stub
-    print filtered_output
+    send_function(filtered)
 
 if __name__ == '__main__':
+    def send_stub(filtered_output):
+        print filtered_output
+
     dacapo_jar = 'dacapo-9.12-bach.jar'
-    run_benchmark('java', 'fop', ['-jar', dacapo_jar])
+    run_benchmark('java', 'fop',
+                  options=['-jar', dacapo_jar],
+                  send_function=send_stub)
