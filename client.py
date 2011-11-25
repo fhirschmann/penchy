047f560b1e32d44b7c4bbcd7a67fd193dc14f79d
Michael Markert <markert.michael@googlemail.com>
2011-11-25 13:40:14 +0100

    [client] Add rudimentary data transmission.
    Server part missing.
    
    Signed-off-by: Michael Markert <markert.michael@googlemail.com>

--- client.py
+++ client.py
@@ -4,23 +4,31 @@
 
 import subprocess
 
+import rpyc
+
 import analyzers
 
-def send_data(filtered_output):
+def send_data(filtered_output, server):
     """
     Send filtered benchmark output to server.
 
     :param filtered_output: relevant benchmark output
+    :param server: server identifier, tuple of (Host, Port), if Port is None
+                   default will be used
     """
-    # XXX: testing stub
-    print filtered_output
+    host, port = server
+    conn =  rpyc.classic.connect(host, port) if port is not None \
+            else \
+            rpyc.classic.connect(host)
+    conn.modules.penchy.server.rcv_data(filtered_output)
 
-def run_benchmark(jvm, benchmark, options=[], send_function=send_data):
+def run_benchmark(jvm, benchmark, server, options=[], send_function=send_data):
     """
     Run :param:`benchmark` on :param:`jvm`.
 
     :param jvm: path to jvm
     :param benchmark: benchmark to execute
+    :param server: server identifier, see `send_data`
     :param options: list of jvm options
     """
     call = [jvm] + options + [benchmark]
@@ -29,13 +37,13 @@ def run_benchmark(jvm, benchmark, options=[], send_function=send_data):
     _, out = proc.communicate()
     analyzer = analyzers.get_analyzer(benchmark)
     filtered = analyzer(out)
-    send_function(filtered)
+    send_function(filtered, server)
 
 if __name__ == '__main__':
-    def send_stub(filtered_output):
+    def send_stub(filtered_output, _):
         print filtered_output
 
     dacapo_jar = 'dacapo-9.12-bach.jar'
-    run_benchmark('java', 'fop',
+    run_benchmark('java', 'fop', ("localhost", 3000),
                   options=['-jar', dacapo_jar],
                   send_function=send_stub)
