60774ca1a336fac5c0d9fb92ab6de509cd92dd43
Michael Markert <markert.michael@googlemail.com>
2011-12-03 20:30:52 +0100

    Add tests for topsort.
    Signed-off-by: Michael Markert <markert.michael@googlemail.com>

---
+++ tests/test_util.py
@@ -0,0 +1,22 @@
+import unittest2
+import util
+
+class TopSortTest(unittest2.TestCase):
+    def test_error(self):
+        a,b = range(2)
+        deps = [(a,b), (b,a)]
+        with self.assertRaises(ValueError):
+            util.topological_sort([], deps)
+
+    def test_multi_deps(self):
+        a,b,c,d = range(4)
+        start = [a,b]
+        deps = [([a,b], c),
+                (c, d)]
+        self.assertTrue(util.topological_sort(start, deps) in ([a,b,c,d],
+                                                              [b,a,c,d]))
+    def test_linear_deps(self):
+        a,b,c,d = range(4)
+        start = [a]
+        deps = [(a,b),(b,c),(c,d)]
+        self.assertListEqual(util.topological_sort(start, deps), range(4))
