96ee0841d0496d2f5d7dd0d78d8359676be1d5ec
Michael Markert <markert.michael@googlemail.com>
2011-12-03 12:54:51 +0100

    Add topological sort.
    Signed-off-by: Michael Markert <markert.michael@googlemail.com>

---
+++ util.py
@@ -0,0 +1,41 @@
+
+def topological_sort(start_nodes, dependencies):
+    """
+    Return a topologically sorted list of :param:`start_nodes` and
+    :param:`dependencies`.
+
+    Nodes are checked on identity, not equality.
+
+    Raises a ValueError if no topological sort is possible.
+
+    :param start_nodes: list of nodes of graph with no incoming edges
+    :param dependencies: list of dependency edges; ([dependencies], target),
+                         dependencies may be of any sequence or be atomic
+    :returns: topologically sorted nodes
+    :rtype: list of nodes
+    """
+    seen = set(start_nodes)
+    for deps, target in dependencies:
+        if not deps:
+            seen.add(target)
+    order = list(seen)
+    old_dependencies = []
+    while True:
+        dependencies = [(deps, target) for deps, target in dependencies if target not in seen]
+        if not dependencies:
+            return order
+        if old_dependencies == dependencies:
+            raise ValueError("no topological sort possible")
+
+        for deps, target in dependencies:
+            # test for sequences
+            try:
+                deps = iter(deps)
+            # atomic object
+            except TypeError:
+                deps = [deps]
+            if all(dep in seen for dep in deps):
+                order.append(target)
+                seen.add(target)
+        # copy dependencies to check progress
+        old_dependencies = list(dependencies)
