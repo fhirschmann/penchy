657f05f06d65b118452d5150761ae5c13e8d5742
Fabian Hirschmann <fabian@hirschm.net>
2011-12-05 14:17:41 +0100

    added bins to zip, fixed typo

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(name='Penchy',
         install_requires=[
             'argparse',
             'paramiko',
-            'pyrcrypto',
+            'pycrypto',
             'rpyc',
             'unittest2',
             ]
