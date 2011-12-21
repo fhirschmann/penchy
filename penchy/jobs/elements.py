743a96c9e4d972cfc502dc91191e00f314114c60
Michael Markert <markert.michael@googlemail.com>
2011-12-21 18:50:23 +0100

    elements: Implement `NotRunnable`.
    Inheritance order of `Tool` and `Workload` had to be changed in order to
    inherit `NotRunnable`s `run`.
    
    Signed-off-by: Michael Markert <markert.michael@googlemail.com>

--- penchy/jobs/elements.py
+++ penchy/jobs/elements.py
@@ -6,18 +6,21 @@ This module provides the foundation of job elements.
 class PipelineElement(object):
     pass
 
-
 class NotRunnable(object):
-    pass
-
+    """
+    This represents a pipeline element that can't be run.
+    """
+    def run(self):
+        # TODO: Add logging?
+        raise ValueError("{0} can't be run!".format(self.__class__.__name__))
 
 class Filter(PipelineElement):
     pass
 
 
-class Tool(PipelineElement, NotRunnable):
+class Tool(NotRunnable, PipelineElement):
     pass
 
 
-class Workload(PipelineElement, NotRunnable):
+class Workload(NotRunnable, PipelineElement):
     pass
