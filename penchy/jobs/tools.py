47e279befb6cb8e69413240aaf473f98c95f93a2
Felix Müller <felix__@web.de>
2011-12-22 16:01:56 +0100

    first version of tamiflex-tool

--- penchy/jobs/tools.py
+++ penchy/jobs/tools.py
@@ -6,7 +6,32 @@ from penchy.jobs.elements import Tool
 
 
 class Tamiflex(Tool):
-    pass
+    """
+    Currently only the play-out-agent is supported.
+    
+    """
+    DEPENDENCIES = set((
+        MavenDependency(
+            # TODO: add dependencies (poa-2.0.0.0.jar)
+        ),
+    ))
+    
+    exports = ["reflection_log", "classfolder"]
+    
+    # TODO: do not set 'out' until the files are created
+    out = {
+        "reflection" : "out/refl.log", 
+            # provides info/log about reflective calls
+        "classfolder" : "out",
+            # contains all classes of which objects were created (?)
+          }
+    
+    def __init__(self):
+        super(Tamiflex, self).__init__()
+    
+    @property
+    def arguments(self):
+        return ["-javaagent:poa-2.0.0.0.jar"]
 
 
 class HProf(Tool):
