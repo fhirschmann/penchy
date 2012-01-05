bb97699b32cef90df860428e8db396a0d4924e12
Michael Markert <markert.michael@googlemail.com>
2012-01-05 12:14:55 +0100

    Fix topsort test.
    Remove of start nodes broke it.
    
    Signed-off-by: Michael Markert <markert.michael@googlemail.com>

--- penchy/tests/test_util.py
+++ penchy/tests/test_util.py
@@ -5,25 +5,26 @@ from penchy import util
 
 class TopSortTest(unittest2.TestCase):
 
-    def test_error(self):
+    def test_circle(self):
         a, b = range(2)
         deps = [(a, b), (b, a)]
         with self.assertRaises(ValueError):
-            util.topological_sort([], deps)
+            util.topological_sort(deps)
 
     def test_multi_deps(self):
         a, b, c, d = range(4)
         start = [a, b]
-        deps = [([a, b], c),
+        deps = [(None, a),
+                (None, b),
+                ([a, b], c),
                 (c, d)]
-        self.assertTrue(util.topological_sort(start, deps) in ([a, b, c, d],
-                                                              [b, a, c, d]))
+        self.assertTrue(util.topological_sort(deps) in ([a, b, c, d],
+                                                       [b, a, c, d]))
 
     def test_linear_deps(self):
         a, b, c, d = range(4)
-        start = [a]
-        deps = [(a, b), (b, c), (c, d)]
-        self.assertListEqual(util.topological_sort(start, deps), range(4))
+        deps = [(None, a), (a, b), (b, c), (c, d)]
+        self.assertListEqual(util.topological_sort(deps), range(4))
 
 
 class ClasspathTest(unittest2.TestCase):
