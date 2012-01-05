8b628ce91040736e1cb33a544871925608c70479
Michael Markert <markert.michael@googlemail.com>
2012-01-05 12:01:57 +0100

    Add edgesort as a frontend to topological_sort.
    Signed-off-by: Michael Markert <markert.michael@googlemail.com>

--- penchy/jobs/dependency.py
+++ penchy/jobs/dependency.py
@@ -3,6 +3,8 @@ This module provides the parts to model and resolve dependencies in the flow of
 execution.
 """
 
+from penchy.util import topological_sort
+
 
 class Edge(object):
     """
@@ -19,3 +21,23 @@ class Edge(object):
         self.source = source
         self.sink = sink
         self.map = map
+
+
+def edgesort(edges):
+    """
+    Return the topological sorted elements of ``edges``.
+
+    :param edges: Sequence of :class:`Edge`
+    :returns: topological sorted :class:`PipelineElement`
+    """
+    starts = set(edge.source for edge in edges)
+    deps = []
+    edges = list(edges)
+    while edges:
+        target = edges[0].sink
+        starts.discard(target)
+        sources = [edge.source for edge in edges if edge.sink is target]
+        deps.append((sources if sources else None, target))
+        edges = [edge for edge in edges if edge.sink is not target]
+    deps.extend((None, start) for start in starts)
+    return topological_sort(deps)
