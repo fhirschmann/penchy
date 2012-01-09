d0a0befcd45abaca918a8845b5cb3ffbb0142079
Michael Markert <markert.michael@googlemail.com>
2012-01-10 00:41:05 +0100

    filters: Add DacapoHarness filter (renamed from Dacapo).
    Signed-off-by: Michael Markert <markert.michael@googlemail.com>

--- penchy/jobs/filters.py
+++ penchy/jobs/filters.py
@@ -2,6 +2,9 @@
 This module provides filters.
 """
 
+import re
+from itertools import izip
+
 from penchy.jobs.elements import Filter
 
 
@@ -12,8 +15,51 @@ class Tamiflex(Filter):
 class HProf(Filter):
     pass
 
-class Dacapo(Filter):
-    pass
+class DacapoHarness(Filter):
+    """
+    Filters output of a DaCapo Harness.
+
+    Inputs:
+    - ``stderr``:  List of Paths to stderror output files, or
+                   list of file-like objects
+    - ``exit_code``: List of program exit codes
+
+    Exports:
+    - ``failures``: failure count per invocation ([int])
+    - ``times``: execution time per itertion per invocation ([[int]])
+    - ``valid``: flag that indicates if execution was valid
+    """
+    inputs = set(('stderror', 'error'))
+    exports = set(('failures', 'times', 'flawed'))
+    TIME_RE = re.compile(
+        r"""
+        (?:completed\ warmup\ \d+|        # for iterations
+        (?P<success>FAILED|PASSED))       # check if run failed or passed
+        \ in\ (?P<time>\d+)\ msec         # time of execution
+        """, re.VERBOSE)
+
+    def run(self, **kwargs):
+        exit_codes = kwargs['exit_code']
+        stderror = kwargs['stderr']
+
+        for f, exit_code in izip(stderror, exit_codes):
+            failures = 0
+            times = []
+            # not file-like
+            if not hasattr(f, 'read'):
+                with open(f) as fobj:
+                    buf = fobj.read()
+            else:
+                buf = f.read()
+            for match in DacapoHarness.TIME_RE.finditer(buf):
+                success, time = match.groups()
+                if success is not None and success == 'FAILED':
+                    failures += 1
+                times.append(int(time))
+
+            self.out['failures'].append(failures)
+            self.out['times'].append(times)
+            self.out['valid'].append(exit_code == 0 and failures == 0)
 
 class Send(Filter):
     pass
