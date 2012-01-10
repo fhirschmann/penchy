e106bf5c6432b7a0389dfec6d52b11fc272c9682
Fabian Hirschmann <fabian@hirschm.net>
2012-01-11 00:01:39 +0100

    making NodeConfig a class instead of a namedtuple

--- penchy/node.py
+++ penchy/node.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python
 
-from collections import namedtuple
+from penchy.util import dict2string
 
 
-NodeConfig = namedtuple('NodeConfig', ['host', 'ssh_port', 'username', 'path'])
+class NodeConfig(object):
+    """
+    This class represents a configuration of a node.
+    """
+
+    def __init__(self, host, ssh_port, username, path):
+        self.host = host
+        self.ssh_port = ssh_port
+        self.username = username
+        self.path = path
+
+    def __str__(self):
+        return "<%s: %s>" % (self.__class__.__name__,
+                dict2string(self.__dict__))
