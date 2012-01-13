19a00942124383d5197a23fdd128a5cd7ae468ab
Michael Markert <markert.michael@googlemail.com>
2012-01-13 21:38:52 +0100

    Disable logging in tests.
    Signed-off-by: Michael Markert <markert.michael@googlemail.com>

--- penchy/log.py
+++ penchy/log.py
@@ -10,3 +10,25 @@ def configure(level=logging.INFO):
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     ch.setFormatter(formatter)
     logging.root.addHandler(ch)
+
+
+def configure_for_tests():
+    """
+    Configure the root logger to be silent.
+    """
+    logging.root.handlers = []
+    logging.root.addHandler(NullHandler())
+
+
+class NullHandler(logging.Handler):
+    """
+    Handler that does nothing.
+    """
+    def handle(self, record):
+        pass
+
+    def emit(self, record):
+        pass
+
+    def createLock(self):
+        self.lock = None
---
+++ penchy/tests/__init__.py
@@ -0,0 +1,5 @@
+from penchy.log import configure_for_tests
+
+configure_for_tests()
+
+del configure_for_tests
