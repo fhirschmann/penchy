306772770851650976cd388b8bb9143c5466362c
Fabian Hirschmann <fabian@hirschm.net>
2012-01-15 22:52:23 +0100

    parsing arguments and loading the job

--- penchy/client.py
+++ penchy/client.py
@@ -2,11 +2,52 @@
 
 import time
 import sys
+import logging
+import imp
 
+import argparse
 import rpyc
 
 
+log = logging.getLogger(__name__)
+
+
 class Client(object):
+    def __init__(self):
+        """
+        Initiates the client.
+        """
+        self.args = None
+        self.job = None
+
+    def run(self):
+        """
+        Runs the client.
+        """
+        self.job.job.run(self.job.jconfig)
+
+    def parse_args(self, args):
+        """
+        Parses the arguments.
+
+        :param argv: arguments; this would normally be sys.argv
+        :type argv: list
+        """
+        parser = argparse.ArgumentParser(description=__doc__, prog=args[0])
+        parser.add_argument("job", help="job to execute", metavar="job")
+        parser.add_argument("-l", "--loglevel", dest="loglevel",
+                default='INFO')
+        self.args = parser.parse_args(args=args[1:])
+
+        try:
+            logging.root.setLevel(getattr(logging, self.args.loglevel))
+        except AttributeError:
+            pass
+
+        log.info('Loading job from %s' % self.args.job)
+        self.job = imp.load_source('job', self.args.job)
+
+    # XXX: Old method; only here for reference
     def send_data(self, filtered_output, server):
         """
         Send filtered benchmark output to server.
@@ -18,6 +59,3 @@ class Client(object):
         host, port = server
         conn = rpyc.connect(host, port)
         conn.root.rcv_data(filtered_output)
-
-    def main(argv):
-        pass
