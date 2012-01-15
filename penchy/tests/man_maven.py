454463be81551d3583f4293a90c57eb7817409a1
Fabian Hirschmann <fabian@hirschm.net>
2012-01-15 20:39:55 +0100

    using setUpClass instead of setUp for speed improvements

--- penchy/tests/man_maven.py
+++ penchy/tests/man_maven.py
@@ -5,13 +5,17 @@ from penchy.tests.unit import unittest
 
 
 class MavenTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.dep = MavenDependency('log4j', 'log4j', '1.2.9')
+        cls.tf = NamedTemporaryFile()
+        cls.dep.pom_path = cls.tf.name
+        cls.pom = POM(groupId='a', artifactId='b', version='1')
+        cls.pom.add_dependency(cls.dep)
+        cls.pom.write(cls.tf.name)
+
     def setUp(self):
-        self.dep = MavenDependency('log4j', 'log4j', '1.2.9')
-        self.tf = NamedTemporaryFile()
         self.dep.pom_path = self.tf.name
-        self.pom = POM(groupId='a', artifactId='b', version='1')
-        self.pom.add_dependency(self.dep)
-        self.pom.write(self.tf.name)
 
     def test_get_classpath(self):
         classpath = get_classpath(self.tf.name)
@@ -30,3 +34,7 @@ class MavenTest(unittest.TestCase):
 
     def test_pom_not_found(self):
         self.assertRaises(OSError, get_classpath, '')
+
+    def test_checksum(self):
+        self.assertEquals(self.dep.actual_checksum,
+                '55856d711ab8b88f8c7b04fd85ff1643ffbfde7c')
