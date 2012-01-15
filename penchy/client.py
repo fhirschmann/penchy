dc43546244e8d234ab478b93b399e0585d1c0f88
Fabian Hirschmann <fabian@hirschm.net>
2012-01-15 22:29:11 +0100

    new template for client

--- penchy/client.py
+++ penchy/client.py
@@ -1,53 +1,23 @@
-#!/usr/bin/env python
-
 """ Executes benchmarks and filters generated data. """
 
-import subprocess
 import time
 import sys
 
 import rpyc
 
-from penchy import analyzers
 
+class Client(object):
+    def send_data(self, filtered_output, server):
+        """
+        Send filtered benchmark output to server.
 
-def send_data(filtered_output, server):
-    """
-    Send filtered benchmark output to server.
+        :param filtered_output: relevant benchmark output
+        :param server: server identifier, tuple of (Host, Port), if Port is None
+                       default will be used
+        """
+        host, port = server
+        conn = rpyc.connect(host, port)
+        conn.root.rcv_data(filtered_output)
 
-    :param filtered_output: relevant benchmark output
-    :param server: server identifier, tuple of (Host, Port), if Port is None
-                   default will be used
-    """
-    host, port = server
-    conn = rpyc.connect(host, port)
-    conn.root.rcv_data(filtered_output)
-
-
-def run_benchmark(jvm, benchmark, server, options=[], send_function=send_data):
-    """
-    Run :param:`benchmark` on :param:`jvm`.
-
-    :param jvm: path to jvm
-    :param benchmark: benchmark to execute
-    :param server: server identifier, see `send_data`
-    :param options: list of jvm options
-    """
-    call = [jvm] + options + [benchmark]
-    proc = subprocess.Popen(call, stderr=subprocess.PIPE)
-    proc.wait()
-    _, out = proc.communicate()
-    analyzer = analyzers.get_analyzer(benchmark)
-    filtered = analyzer(out)
-    send_function(filtered, server)
-
-
-def main(argv=None):
-    time.sleep(1)
-
-    dacapo_jar = 'dacapo-9.12-bach.jar'
-    run_benchmark('java', 'fop', ("192.168.56.1", 4343),
-                  options=['-jar', dacapo_jar])
-
-if __name__ == '__main__':
-    main(sys.argv)
+    def main(argv):
+        pass
