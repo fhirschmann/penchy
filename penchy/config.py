c50917a602ebbf2ad302a5703b2e240afd9dfa9b
Fabian Hirschmann <fabian@hirschm.net>
2012-01-16 23:46:02 +0100

    moving NodeConfiguration to penchy.config

--- penchy/config.py
+++ penchy/config.py
@@ -2,7 +2,49 @@
 
 import os
 
-from penchy.node import NodeConfiguration
+
+class NodeConfiguration(object):
+    """
+    This class represents a configuration of a node.
+    """
+
+    def __init__(self, host, ssh_port, username, path,
+                 basepath, description="", password=None,
+                 keyfile=None):
+        """
+        :param host: hostname (or IP) of node
+        :type host: string
+        :param ssh_port: port number of ssh server on node
+        :type ssh_port: int
+        :param username: login name for penchy on node
+        :type username: string
+        :param path: working directory on the node (this is where
+                     the job will be uploaded to and where the
+                     temporary files and directories will be created)
+        :type path: string
+        :param basepath: basepath for JVMs on this node
+        :type basepath: string
+        :param description: Textual description of node
+        :type description: string
+        :param password: this is either the password for a given username
+                         or the passphrase to unlock the keyfile
+        :type password: string
+        :param keyfile: path to the ssh keyfile to use
+        :type keyfile: string
+        """
+        self.host = host
+        self.ssh_port = ssh_port
+        self.username = username
+        self.path = path
+        self.basepath = basepath
+        self.description = description
+        self.password = password
+        self.keyfile = keyfile
+
+    def __str__(self):
+        return "<%s: %s>" % (self.__class__.__name__,
+                dict2string(self.__dict__, ['host', 'ssh_port']))
+
 
 # The location of your private ssh key
 ID_RSA = os.path.expanduser('~/.ssh/id_rsa')
--- penchy/node.py
+++ penchy/node.py
@@ -12,49 +12,6 @@ from penchy.util import dict2string
 log = logging.getLogger(__name__)
 
 
-class NodeConfiguration(object):
-    """
-    This class represents a configuration of a node.
-    """
-
-    def __init__(self, host, ssh_port, username, path,
-                 basepath, description="", password=None,
-                 keyfile=None):
-        """
-        :param host: hostname (or IP) of node
-        :type host: string
-        :param ssh_port: port number of ssh server on node
-        :type ssh_port: int
-        :param username: login name for penchy on node
-        :type username: string
-        :param path: working directory on the node (this is where
-                     the job will be uploaded to and where the
-                     temporary files and directories will be created)
-        :type path: string
-        :param basepath: basepath for JVMs on this node
-        :type basepath: string
-        :param description: Textual description of node
-        :type description: string
-        :param password: this is either the password for a given username
-                         or the passphrase to unlock the keyfile
-        :type password: string
-        :param keyfile: path to the ssh keyfile to use
-        :type keyfile: string
-        """
-        self.host = host
-        self.ssh_port = ssh_port
-        self.username = username
-        self.path = path
-        self.basepath = basepath
-        self.description = description
-        self.password = password
-        self.keyfile = keyfile
-
-    def __str__(self):
-        return "<%s: %s>" % (self.__class__.__name__,
-                dict2string(self.__dict__, ['host', 'ssh_port']))
-
-
 class NodeError(Exception):
     """
     Raised when errors occur while dealing
@@ -77,7 +34,6 @@ class Node(object):
         :param node: the node configuration
         :type node: :class:`NodeConfiguration`
         """
-
         self.config = configuration
         self.ssh = paramiko.SSHClient()
         self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
