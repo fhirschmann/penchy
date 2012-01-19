4587c59360fa1b8ae7e2de14b630e1e705b4a4a5
Fabian Hirschmann <fabian@hirschm.net>
2012-01-19 11:56:52 +0100

    moving argument parsing to server module

--- penchy/server.py
+++ penchy/server.py
@@ -3,6 +3,7 @@ Initiates multiple JVM Benchmarks and accumulates the results.
 """
 
 import os
+import sys
 import imp
 import atexit
 import logging
@@ -36,24 +37,102 @@ class Server:
     """
     This class represents the server.
     """
-    def __init__(self, config, jobfile):
+    def __init__(self, args):
         """
-        :param config: a config module
-        :param job: the job to execute
-        :type jobfile: string
+        :param args: arguments; this would normally be sys.argv
+        :type args: list
         """
-        self.config = config
-        self.jobfile = jobfile
-        self.job = imp.load_source('job', jobfile)
-        self.nodes = [Node(n, self.job) for n in config.NODES]
-        self.uploads = set((self.jobfile, find_bootstrap_client()))
+        args = self.parse_args(args)
+        self.config, config_filename = self._load_config(args.config)
+        self.job = self._load_job(args.job)
+
+        self.nodes = [Node(n, self.job) for n in self.config.NODES]
+        self.uploads = set((args.job, config_filename, find_bootstrap_client()))
         self.listener = ThreadedServer(Service, hostname="192.168.56.1",
                 port=self.config.LISTEN_PORT)
-        self.client_thread = threading.Thread(target=self.run_clients)
-        self.client_thread.daemon = True
+        self.client_thread = self._setup_client_thread([args.job])
         self.bootstrap_args = []
 
-    def run_clients(self):
+    def _load_config(self, filename):
+        """
+        Loads the config module from filename. Looks
+        in the current working directory as well.
+
+        :param filename: filename of the config file
+        :type filename: string
+        :returns: tuple of (config object, config filename)
+        :rtype: tuple
+        """
+        try:
+            config = imp.load_source('config', filename)
+            actual_filename = filename
+        except IOError:
+            try:
+                config = imp.load_source('config', 'penchyrc')
+                actual_filename = 'penchyrc'
+            except IOError:
+                raise IOError("Config file could not be loaded from: %s or ./penchyrc" % filename)
+
+        sys.modules['config'] = config
+
+        log.info('Using %s config module' % config.__file__)
+        return (config, actual_filename)
+
+    def _load_job(self, filename):
+        """
+        Loads a job.
+
+        :param filename: filename of the job
+        :type filename: string
+        """
+        job = imp.load_source('job', filename)
+        return job
+
+    def _setup_client_thread(self, args):
+        """
+        Sets up the client threads.
+
+        :param args: arguments to pass to run_clients()
+        :type args: list
+        :returns: the thread object
+        :rtype: :class:`threading.Thread`
+        """
+        thread = threading.Thread(target=self.run_clients, args=args)
+        thread.daemon = True
+        return thread
+
+    def parse_args(self, args):
+        """
+        Parses the arguments.
+
+        :param args: arguments; this would normally be sys.argv
+        :type args: list
+        """
+        parser = argparse.ArgumentParser()
+        log_group = parser.add_mutually_exclusive_group()
+        log_group.add_argument("-d", "--debug",
+                action="store_const", const=logging.DEBUG,
+                dest="loglevel", default=logging.INFO,
+                help="print debugging messages")
+        log_group.add_argument("-q", "--quiet",
+                action="store_const", const=logging.WARNING,
+                dest="loglevel", help="suppress most messages")
+        parser.add_argument("-c", "--config",
+                action="store", dest="config",
+                default=os.path.expanduser("~/.penchyrc"),
+                help="config module to use")
+        parser.add_argument("-f", "--load-from",
+                action="store", dest="load_from",
+                help="load penchy from this path on the node")
+        parser.add_argument("job", help="job to execute",
+                metavar="job")
+        args = parser.parse_args(args)
+        logging.root.setLevel(args.loglevel)
+        if args.load_from:
+            self.bootstrap_args.extend(['--load-from', args.load_from])
+        return args
+
+    def run_clients(self, jobfile):
         """
         This method will run the clients on all nodes.
         """
@@ -66,7 +145,7 @@ class Server:
 
                 node.execute_penchy(" ".join(
                     self.bootstrap_args + \
-                    [self.jobfile, "192.168.56.1", "4343", node.identifier]))
+                    [jobfile, "192.168.56.1", "4343", node.identifier]))
                 node.disconnect()
 
     def run(self):
