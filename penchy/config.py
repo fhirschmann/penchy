2258d909073724256fcc4274ed82ca4b66ffb79c
Fabian Hirschmann <fabian@hirschm.net>
2012-01-19 10:56:17 +0100

    moving config stuff to config file

--- penchy/config.py
+++ penchy/config.py
@@ -46,26 +46,3 @@ class NodeConfiguration(object):
     def __str__(self):
         return "<%s: %s>" % (self.__class__.__name__,
                 dict2string(self.__dict__, ['host', 'ssh_port']))
-
-
-# The location of your private ssh key
-ID_RSA = os.path.expanduser('~/.ssh/id_rsa')
-
-# The default username on the nodes
-USERNAME = 'bench'
-
-# A list of files to copy to the node
-FILES = []
-
-SSH_PORT = 22
-
-# The port the server listens on
-LISTEN_PORT = 4343
-
-# List of nodes
-NODES = [
-    NodeConfiguration('192.168.56.11', SSH_PORT, USERNAME, '/home/bench', '/usr/bin'),
-    NodeConfiguration('192.168.56.10', SSH_PORT, USERNAME, '/home/bench', '/usr/bin')
-]
-
-LOCALNODE = NodeConfiguration('localhost', 22, os.environ['USER'], '/tmp', '/usr/bin')
