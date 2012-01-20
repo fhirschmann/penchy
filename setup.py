4cb8e24bc4cc0e52080f3c6bf27130b4bfab03fb
Fabian Hirschmann <fabian@hirschm.net>
2012-01-20 15:18:22 +0100

    fixed setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,20 @@
-#!/usr/bin/env python
-
 from distutils.core import setup
 from setuptools import find_packages
 from penchy import __version__
 
-# Version will be replaced by maven!
 setup(name='penchy',
         version=__version__,
         description='JVM Benchmarking Tool',
         url='http://www.tu-darmstadt.de/',
-        packages=find_packages(),
-        scripts=['bin/penchy_client', 'bin/penchy_server'],
+        packages=['penchy'],
+        scripts=[
+            'bin/penchy',
+            'bin/penchy_test_job,
+            'bin/penchy_bootstrap'],
         install_requires=[
             'argparse',
             'paramiko',
             'pycrypto',
             'rpyc',
-            'unittest2',
-            ]
+            'unittest2']
         )
