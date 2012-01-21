c89f0412b8bd9a4f6f0b2c6d9f46a73992289dfb
Michael Markert <markert.michael@googlemail.com>
2012-01-21 15:19:35 +0100

    Add not equal to NodeConfiguration.
    Signed-off-by: Michael Markert <markert.michael@googlemail.com>

--- penchy/config.py
+++ penchy/config.py
@@ -54,6 +54,9 @@ class NodeConfiguration(object):
         return isinstance(other, NodeConfiguration) and \
                 self.identifier == other.identifier
 
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
     def __hash__(self):
         return hash(self.identifier)
 
