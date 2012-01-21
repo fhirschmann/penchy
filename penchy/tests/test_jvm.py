d32e9fe3928cea980ac682df80b19f67179417a5
Michael Markert <markert.michael@googlemail.com>
2012-01-22 00:48:13 +0100

    tests: Split big jvm test case.
    Signed-off-by: Michael Markert <markert.michael@googlemail.com>

--- penchy/tests/test_jvm.py
+++ penchy/tests/test_jvm.py
@@ -5,15 +5,17 @@ from penchy.jobs.workloads import ScalaBench
 from penchy.tests.util import MockPipelineElement
 
 
-class JVMTest(unittest.TestCase):
+def setup_jvm(self):
+    self.jvm = JVM('java')
+    self.w = ScalaBench('batik')
+    self.jvm.workload = self.w
+    self.t = HProf('')
+    self.jvm.tool = self.t
+    self.jvm.add_to_cp('foo')
 
-    def setUp(self):
-        self.jvm = JVM('java')
-        self.w = ScalaBench('batik')
-        self.jvm.workload = self.w
-        self.t = HProf('')
-        self.jvm.tool = self.t
-        self.jvm.add_to_cp('foo')
+
+class JVMClasspathTest(unittest.TestCase):
+    setUp = setup_jvm
 
     def test_add_to_existing_classpath(self):
         self.jvm._classpath = 'bar'
@@ -30,6 +32,20 @@ class JVMTest(unittest.TestCase):
         self.assertEqual(self.jvm._classpath, 'foo')
         self.jvm.add_to_cp('baz')
         self.assertEqual(self.jvm._classpath, 'foo:baz')
+
+    def test_raise_nonconfigured_workload(self):
+        self.jvm.workload = None
+        with self.assertRaises(JVMNotConfiguredError):
+            self.jvm.run()
+
+    def test_raise_nonconfigured_classpath(self):
+        self.jvm._classpath = None
+        with self.assertRaises(JVMNotConfiguredError):
+            self.jvm.run()
+
+
+class JVMCmdlineTest(unittest.TestCase):
+    setUp = setup_jvm
 
     def test_cmdline_no_classpath(self):
         self.jvm._classpath = ''
@@ -53,6 +69,10 @@ class JVMTest(unittest.TestCase):
         self.assertListEqual(self.jvm.cmdline,
                              ['/java'] + self.t.arguments +
                              ['-classpath', 'foo'] + self.w.arguments)
+
+
+class JVMHooksTest(unittest.TestCase):
+    setUp = setup_jvm
 
     def test_get_hooks_empty(self):
         jvm = JVM('foo')
@@ -79,13 +99,3 @@ class JVMTest(unittest.TestCase):
         jvm.tool = t
         self.assertListEqual(list(map(list, jvm._get_hooks())), [t.prehooks,
                                                                  t.posthooks])
-
-    def test_raise_nonconfigured_workload(self):
-        self.jvm.workload = None
-        with self.assertRaises(JVMNotConfiguredError):
-            self.jvm.run()
-
-    def test_raise_nonconfigured_classpath(self):
-        self.jvm._classpath = None
-        with self.assertRaises(JVMNotConfiguredError):
-            self.jvm.run()
