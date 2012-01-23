9cf98e9c5efb7784a3cc572ea2f7ac684d11a38d
Fabian Hirschmann <fabian@hirschm.net>
2012-01-23 12:52:46 +0100

    fixed run_server_pipeline call

--- penchy/server.py
+++ penchy/server.py
@@ -127,4 +127,4 @@ class Server(object):
         starts the serverside pipeline.
         """
         log.info(Server.results)
-        self.job.run_server_pipeline()
+        self.job.job.run_server_pipeline()
