dfe86401431c5769bbe02d9ed6ef86e049d47afb
Michael Markert <markert.michael@googlemail.com>
2012-01-28 04:37:52 +0100

    tests: Add check tests.
    Signed-off-by: Michael Markert <markert.michael@googlemail.com>

--- penchy/tests/test_job.py
+++ penchy/tests/test_job.py
@@ -2,11 +2,11 @@ from hashlib import sha1
 
 from penchy.compat import unittest, update_hasher
 from penchy.jobs.dependency import Edge
-from penchy.jobs.filters import Print, DacapoHarness, Receive
+from penchy.jobs.filters import Print, DacapoHarness, Receive, Send
 from penchy.jobs.job import Job, SystemComposition, NodeSetting
 from penchy.jobs.jvms import JVM
 from penchy.jobs.tools import HProf
-from penchy.jobs.typecheck import Types, TypeCheckError
+from penchy.jobs.typecheck import Types
 from penchy.jobs.workloads import ScalaBench
 from penchy.tests.util import MockPipelineElement, make_system_composition
 
@@ -58,83 +58,6 @@ class JobServerElementsTest(unittest.TestCase):
 
     def test_empty_elements(self):
         self.assertSetEqual(self.job._get_server_dependencies(), set())
-
-
-class CheckArgsTest(unittest.TestCase):
-    def setUp(self):
-        super(CheckArgsTest, self).setUp()
-
-        self.p = MockPipelineElement()
-        self.inputs = Types(('foo', str),
-                            ('bar', list, int))
-
-        self.d = {'foo' : '23', 'bar' : range(5)}
-
-    def test_malformed_types(self):
-        for t in ((1, str),
-                 (1, list, str),
-                 ('bar', str, 2),
-                 ('baz', 2, list),
-                 ()):
-            with self.assertRaises(AssertionError):
-                Types(t)
-
-    def test_wrong_type(self):
-        self._raising_error_on_replacement(TypeCheckError,
-                                           (('foo', 23),
-                                            ('foo', ['23']),
-                                            ('bar', 42)))
-
-    def test_wrong_subtype(self):
-        self._raising_error_on_replacement(TypeCheckError,
-                                           (('bar', ['23']),
-                                            ('bar', [(), ()])))
-
-    def test_missing_arg(self):
-        self._raising_error_on_deletion(TypeCheckError,
-                                        (('foo'),
-                                         ('bar'),
-                                         (['foo', 'bar']),
-                                         (['foo', 'bar'])))
-
-    def test_unused_input_count(self):
-        self.d['baz'] = 42
-        self.d['bad'] = 23
-        self.assertEqual(self.inputs.check_input(self.d), 2)
-
-    def test_fully_used_input_count(self):
-        self.assertEqual(self.inputs.check_input(self.d), 0)
-
-    def test_disabled_checking(self):
-        # d contains 2 unused inputs
-        self.assertEqual(Types().check_input(self.d), 0)
-
-    def test_subtype_of_dict(self):
-        inputs = Types(('foo', dict, int),
-                       ('bar', dict, list, int))
-        self.assertEqual(inputs.check_input({'foo' : dict(a=1, b=2),
-                                             'bar' : dict(a=[1], b=[2])})
-                         , 0)
-        with self.assertRaises(TypeCheckError):
-            self.inputs.check_input({'foo' : dict(a=1, b=2),
-                                     'bar' : dict(a=1, b=2)})
-
-    def _raising_error_on_deletion(self, error, deletions):
-        for del_ in deletions:
-            with self.assertRaises(error):
-                d = self.d.copy()
-                if not isinstance(del_, (list, tuple)):
-                    del_ = [del_]
-                for del__ in del_:
-                    d.pop(del__, None)
-                self.inputs.check_input(d)
-
-    def _raising_error_on_replacement(self, error, replacements):
-        for k, v in replacements:
-            with self.assertRaises(error):
-                d = self.d.copy()
-                d[k] = v
-                self.inputs.check_input(d)
 
 
 class SystemCompositionsTest(unittest.TestCase):
@@ -246,3 +169,75 @@ class RunServerPipelineTest(unittest.TestCase):
         j = Job([], [], [])
         with self.assertRaises(ValueError):
             j.run_server_pipeline()
+
+
+class JobCheckTest(unittest.TestCase):
+    def test_valid_job(self):
+        c = make_system_composition()
+        w = ScalaBench('jython')
+        f = DacapoHarness()
+        s = Send()
+        r = Receive()
+        p = Print()
+        c.jvm.workload = w
+        j = Job(c,
+                [w >> f >> ('times', 'payload') >> s],
+                [r >> p])
+
+        self.assertTrue(j.check())
+
+    def test_client_cycle(self):
+        c = make_system_composition()
+        w = ScalaBench('jython')
+        f = DacapoHarness()
+        s = Send()
+        r = Receive()
+        p = Print()
+        c.jvm.workload = w
+        j = Job(c, [f >> f >> s], [r >> p])
+        self.assertFalse(j.check())
+
+    def test_server_cycle(self):
+        c = make_system_composition()
+        w = ScalaBench('jython')
+        f = DacapoHarness()
+        s = Send()
+        r = Receive()
+        p = Print()
+        m = MockPipelineElement()
+        m.inputs = Types(('a', int))
+        m.outputs = Types(('a', int))
+        c.jvm.workload = w
+        j = Job(c, [w >> f >> s], [r >> m >> m >> p])
+        self.assertFalse(j.check())
+
+    def test_no_workload(self):
+        c = make_system_composition()
+        j = Job(c, [], [])
+        self.assertFalse(j.check())
+
+    def test_wrong_output(self):
+        c = make_system_composition()
+        w = ScalaBench('jython')
+        f = DacapoHarness()
+        s = Send()
+        r = Receive()
+        p = Print()
+        c.jvm.workload = w
+        j = Job(c,
+                [w >> [('a', 'stderr'), 'exit_code'] >> f >> s],
+                [r >> p])
+        self.assertFalse(j.check())
+
+    def test_missing_input(self):
+        c = make_system_composition()
+        w = ScalaBench('jython')
+        f = DacapoHarness()
+        s = Send()
+        r = Receive()
+        p = Print()
+        c.jvm.workload = w
+        j = Job(c,
+                [w >> 'exit_code' >> f >> s],
+                [r >> p])
+        self.assertFalse(j.check())
---
+++ penchy/tests/test_typecheck.py
@@ -0,0 +1,80 @@
+from penchy.compat import unittest
+from penchy.jobs.typecheck import Types, TypeCheckError
+from penchy.tests.util import MockPipelineElement
+
+
+class CheckArgsTest(unittest.TestCase):
+    def setUp(self):
+        super(CheckArgsTest, self).setUp()
+
+        self.p = MockPipelineElement()
+        self.inputs = Types(('foo', str),
+                            ('bar', list, int))
+
+        self.d = {'foo' : '23', 'bar' : range(5)}
+
+    def test_malformed_types(self):
+        for t in ((1, str),
+                 (1, list, str),
+                 ('bar', str, 2),
+                 ('baz', 2, list),
+                 ()):
+            with self.assertRaises(AssertionError):
+                Types(t)
+
+    def test_wrong_type(self):
+        self._raising_error_on_replacement(TypeCheckError,
+                                           (('foo', 23),
+                                            ('foo', ['23']),
+                                            ('bar', 42)))
+
+    def test_wrong_subtype(self):
+        self._raising_error_on_replacement(TypeCheckError,
+                                           (('bar', ['23']),
+                                            ('bar', [(), ()])))
+
+    def test_missing_arg(self):
+        self._raising_error_on_deletion(TypeCheckError,
+                                        (('foo'),
+                                         ('bar'),
+                                         (['foo', 'bar']),
+                                         (['foo', 'bar'])))
+
+    def test_unused_input_count(self):
+        self.d['baz'] = 42
+        self.d['bad'] = 23
+        self.assertEqual(self.inputs.check_input(self.d), 2)
+
+    def test_fully_used_input_count(self):
+        self.assertEqual(self.inputs.check_input(self.d), 0)
+
+    def test_disabled_checking(self):
+        # d contains 2 unused inputs
+        self.assertEqual(Types().check_input(self.d), 0)
+
+    def test_subtype_of_dict(self):
+        inputs = Types(('foo', dict, int),
+                       ('bar', dict, list, int))
+        self.assertEqual(inputs.check_input({'foo' : dict(a=1, b=2),
+                                             'bar' : dict(a=[1], b=[2])})
+                         , 0)
+        with self.assertRaises(TypeCheckError):
+            self.inputs.check_input({'foo' : dict(a=1, b=2),
+                                     'bar' : dict(a=1, b=2)})
+
+    def _raising_error_on_deletion(self, error, deletions):
+        for del_ in deletions:
+            with self.assertRaises(error):
+                d = self.d.copy()
+                if not isinstance(del_, (list, tuple)):
+                    del_ = [del_]
+                for del__ in del_:
+                    d.pop(del__, None)
+                self.inputs.check_input(d)
+
+    def _raising_error_on_replacement(self, error, replacements):
+        for k, v in replacements:
+            with self.assertRaises(error):
+                d = self.d.copy()
+                d[k] = v
+                self.inputs.check_input(d)
