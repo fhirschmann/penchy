d7bd6299a826cdd206a36f7bce967e4e8534e2e0
Pascal Wittmann <mail@pascal-wittmann.de>
2012-02-01 10:56:55 +0100

    plot: added barchart example for matplotlib

---
+++ examples/barplot.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+openJDK = (21, 42, 32)
+hotspot = (20, 43, 32)
+
+N = len(openJDK)
+
+ind = np.arange(N)
+width = 0.35
+
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
+
+rect1 = ax.bar(ind, openJDK, width, color='blue')
+rect2 = ax.bar(ind+width, hotspot, width, color='red')
+
+ax.set_ylabel('Wallclocktime')
+ax.set_title('openJDK vs. hotspot')
+ax.set_xticks(ind+width)
+ax.set_xticklabels( ('fop', 'batik', 'eclipse') )
+ax.legend( (rect1[0], rect2[0]) , ('openJDK', 'hotspot') )
+
+plt.show()
