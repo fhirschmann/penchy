d44b2f7a6e566f2234d87384d8653b68be6ba20c
Fabian Hirschmann <fabian@hirschm.net>
2012-02-03 15:17:03 +0100

    better error messages

--- penchy/apicheck.py
+++ penchy/apicheck.py
@@ -14,8 +14,20 @@ log = logging.getLogger(__name__)
 def compare_argspec(func, expected):
     actual = inspect.getargspec(func)
     if not expected == actual:
-        log.warning("Arguments for %s have changed:\nExpected:\n%s\nActual:\n%s" % \
-                (func, expected, actual))
+        log.warning("Arguments for %s in %s have changed:\nExpected:\n%s\nActual:\n%s" % \
+                (func.__name__, func.__module__, expected, actual))
+        return True
+    else:
+        return False
+
+
+def checkattr(func, attr):
+    if not hasattr(func, attr):
+        log.error("%s in %s no longer has a %s attribute" % (func.__name__,
+            func.__module__, attr))
+        return True
+    else:
+        return False
 
 
 def check_paramiko():
@@ -29,24 +41,24 @@ def check_paramiko():
         log.error("Could not import paramiko - did you install it?")
         raise
 
-    getattr(paramiko, 'SSHClient')
-    getattr(paramiko.SSHClient, 'open_sftp')
-    getattr(paramiko.SFTPClient, 'open')
+    checkattr(paramiko, 'SSHClient')
+    checkattr(paramiko.SSHClient, 'open_sftp')
+    checkattr(paramiko.SFTPClient, 'open')
+    checkattr(paramiko.SSHClient, 'connect')
+    checkattr(paramiko.SFTPClient, 'put')
+    checkattr(paramiko.SFTPClient, 'mkdir')
 
-    getattr(paramiko.SSHClient, 'connect')
     expected = ArgSpec(args=['self', 'hostname', 'port', 'username',
         'password', 'pkey', 'key_filename', 'timeout', 'allow_agent',
         'look_for_keys', 'compress'], varargs=None, keywords=None,
         defaults=(22, None, None, None, None, None, True, True, False))
     compare_argspec(paramiko.SSHClient.connect, expected)
 
-    getattr(paramiko.SFTPClient, 'put')
     expected = ArgSpec(args=['self', 'localpath', 'remotepath',
         'callback', 'confirm'], varargs=None, keywords=None,
         defaults=(None, True))
     compare_argspec(paramiko.SFTPClient.put, expected)
 
-    getattr(paramiko.SFTPClient, 'mkdir')
     expected = ArgSpec(args=['self', 'path', 'mode'], varargs=None,
             keywords=None, defaults=(511,))
     compare_argspec(paramiko.SFTPClient.mkdir, expected)
