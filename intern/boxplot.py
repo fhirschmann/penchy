41de5903ad573877779bc0079062034cd08fe438
Felix Müller <felix__@web.de>
2012-02-12 19:13:43 +0100

    minor corrections + labels on x-axes

--- intern/boxplot.py
+++ intern/boxplot.py
@@ -4,14 +4,14 @@ import matplotlib.pyplot as plt
 import random
 
 # generate random data
+names = ["aaa", "bbb", "ccc"]
 random.seed()
 data = []
-for i in range(3):
+for i in range(len(names)):
     set_ = []
     for j in range(30):
         set_.append(random.randint(1, 50))
     data.append(set_)
-    set_ = []
 
 # print data for comparison
 for set_ in data:
@@ -20,11 +20,12 @@ for set_ in data:
     print "data:", sorted_set
     print "upper quantile:", sorted_set[(length * 3) / 4]
     print "median:        ", sorted_set[length / 2]
-    print "upper quantile:", sorted_set[length / 4]
+    print "lower quantile:", sorted_set[length / 4]
 
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
 
 ax.boxplot(data)
+ax.set_xticklabels(names)
 
 plt.show()
