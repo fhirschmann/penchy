b290c6b665cbbfb98d89d31a1b17c8ae42bd3b4a
Pascal Wittmann <mail@pascal-wittmann.de>
2012-02-17 00:16:15 +0100

    typecheck: made change consistent

--- penchy/jobs/typecheck.py
+++ penchy/jobs/typecheck.py
@@ -141,7 +141,8 @@ class Types(object):
         if mapping is not None:
             sinks = map(lambda s: s[1], mapping)
             if len(sinks) != len(set(sinks)):
-                log.error('Multiple sources are connected to the same sink input')
+                log.error('Multiple source outputs are connected to the' +
+                          ' same sink input')
                 valid = False
 
         if self.descriptions is not None:
