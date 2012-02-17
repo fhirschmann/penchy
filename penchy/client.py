4161b638248ad8de386372cc4b4fe934248051c5
Fabian Hirschmann <fabian@hirschm.net>
2012-02-17 11:53:30 +0100

    added signal handler stub

--- penchy/client.py
+++ penchy/client.py
@@ -9,6 +9,7 @@ Executes benchmarks and filters generated data.
 
 import logging
 import xmlrpclib
+import signal
 
 import argparse
 
@@ -37,10 +38,25 @@ class Client(object):
         self.identifier = self.args.identifier
         self.proxy = xmlrpclib.ServerProxy("http://%s:%s/" % \
                 (self.config.SERVER_HOST, self.config.SERVER_PORT))
+        self._current_composition = None
 
         configure_logging(int(self.args.loglevel))
 
-        # TODO: Implement signal handler for timeouts
+        signal.signal(signal.SIGHUP, self._signal_handler)
+
+    def _signal_handler(self, signum, frame):
+        """
+        Handles signals sent to this process.
+
+        :param signum: signal number as defined in the ``signal`` module
+        :type signum: int
+        :param frame: execution frame
+        :type frame: frame object
+        """
+        log.info("Received signal %s " % signum)
+        if signum == signal.SIGHUP:
+            # TODO: Kill self._current_composition.jvm
+            pass
 
     def run(self):
         """
@@ -51,7 +67,9 @@ class Client(object):
 
         for composition in self.job.compositions_for_node(self.identifier):
             try:
+                self._current_composition = composition
                 self.job.run(composition)
+                self._current_composition = None
             except Exception, err:
                 log.exception('Exception occured while executing PenchY:')
                 self.proxy.node_error(composition.hash(), err)
