a592a6f53c39dfd07a8ff30455c9245b842ca952
Pascal Wittmann <mail@pascal-wittmann.de>
2012-02-18 17:28:37 +0100

    plots: moved plot code to separate module

--- penchy/jobs/__init__.py
+++ penchy/jobs/__init__.py
@@ -14,6 +14,7 @@ __all__ = [
     # modules
     'jvms',
     'filters',
+    'plots',
     'workloads',
     'tools'
 ]
--- penchy/jobs/filters.py
+++ penchy/jobs/filters.py
@@ -467,48 +467,6 @@ class CondensingReceive(Receive, Condense):
         Condense._run(self, results=results)
 
 
-class Plot(Filter):
-    pass
-
-
-class BarPlot(Filter):
-    """
-    A simple and static barplot.  Mainly for testing.
-    """
-    inputs = Types(('x', list, str), ('y', list, list))
-
-    def __init__(self, filename, title="", xlabel="", ylabel="", colors=[]):
-        super(BarPlot, self).__init__()
-        self.filename = filename
-        self.title = title
-        self.xlabel = xlabel
-        self.ylabel = ylabel
-        self.width = 0.2
-        self.colors = colors
-
-    def _run(self, **kwargs):
-        import numpy as np
-        import matplotlib.pyplot as plt
-        xs = kwargs['x']
-        yss = kwargs['y']
-        ind = np.arange(len(xs))
-        fig = plt.figure()
-        plot = fig.add_subplot(1, 1, 1)
-        bars, names, rects = [], [], []
-        for i, ys, c in zip(itertools.count(), zip(*yss), self.colors):
-            rects.append(plot.bar(ind + self.width * i, [average(y) for y in ys],
-                                  self.width, color=c))
-            bars.append(rects[i][0])
-            names.append('Invocation {0}'.format(i + 1))
-        plot.set_xlabel(self.xlabel)
-        plot.set_ylabel(self.ylabel)
-        plot.set_title(self.title)
-        plot.set_xticks(ind + self.width)
-        plot.set_xticklabels(xs)
-        plot.legend(bars, names)
-        plt.savefig(self.filename)
-
-
 class Upload(Filter):
     pass
 
