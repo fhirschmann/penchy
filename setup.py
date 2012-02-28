75a1f20b7e17d887c72a2ee6c034196e688fcda1
Fabian Hirschmann <fabian@hirschm.net>
2012-02-28 14:14:57 +0100

    fixed typo; removed unused import

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 from distutils.core import setup
-from setuptools import find_packages
 from penchy import __version__
 
 setup(name='penchy',
@@ -9,7 +8,7 @@ setup(name='penchy',
         packages=['penchy'],
         scripts=[
             'bin/penchy',
-            'bin/penchy_test_job,
+            'bin/penchy_test_job',
             'bin/penchy_bootstrap'],
         install_requires=[
             'numpy'
