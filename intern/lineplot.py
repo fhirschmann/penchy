7cf529f8fbb52c145c3a3dae5e3aebd2db330fc1
Pascal Wittmann <mail@pascal-wittmann.de>
2012-02-28 17:30:16 +0100

    intern: fixed lineplot example

--- intern/lineplot.py
+++ intern/lineplot.py
@@ -6,7 +6,7 @@ data = {
     'metric a':
         [22, 3, 2, 12, 3, 13, 11, 23, 21, 12, 23, 24],
     'metric b':
-        [2, 12, 1, 2, 12, 21, 12, 3, 23, 32, 31, 21, 3]
+        [2, 12, 1, 2, 12, 21, 12, 3, 23, 32, 31, 21]
     }
 
 fig = plt.figure()
@@ -15,8 +15,7 @@ ax = fig.add_subplot(1, 1, 1)
 legend_lines = []
 
 for key in data.keys():
-    legend_lines.append(ax.plot(data[key]))
+    legend_lines.append(ax.plot(data[key])[0])
     
 ax.legend(legend_lines, data.keys())
-
 plt.show()
