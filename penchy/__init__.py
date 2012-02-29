d79da4c86eb2e9d8776fe651ad45f797b1204be6
Fabian Hirschmann <fabian@hirschm.net>
2012-02-29 13:30:20 +0100

    introduced penchy.is_client and penchy.is_server

--- penchy/__init__.py
+++ penchy/__init__.py
@@ -1,2 +1,11 @@
+import sys
+
 # Version will be replaced by maven!
 __version__ = '0.1'
+
+if sys.argv[0].endswith('penchy'):
+    is_server = True
+else:
+    is_server = False
+
+is_client = not is_server
