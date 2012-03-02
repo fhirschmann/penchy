c71a6cf6a317ca48ebed091a19e9720235e788bb
Fabian Hirschmann <fabian@hirschm.net>
2012-03-02 02:13:56 +0100

    removed stupidity

--- penchy/__init__.py
+++ penchy/__init__.py
@@ -9,9 +9,5 @@ import sys
 # Version will be replaced by maven!
 __version__ = '0.1'
 
-if sys.argv[0].endswith('penchy'):
-    is_server = True
-else:
-    is_server = False
-
+is_server = sys.argv[0].endswith('penchy')
 is_client = not is_server
