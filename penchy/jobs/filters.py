8ea60ed2154bc764fd656a80a87498739e335e10
Pascal Wittmann <mail@pascal-wittmann.de>
2012-03-05 14:50:42 +0100

    filters: fixed ConfidenceIntervalTwo

--- penchy/jobs/filters.py
+++ penchy/jobs/filters.py
@@ -991,6 +991,9 @@ class ConfidenceIntervalTwo(Filter):
         s1 = sample_standard_deviation(xs)
         s2 = sample_standard_deviation(ys)
         sx = math.sqrt((s1 ** 2) / n1 + (s2 ** 2) / n2)
+        avgx = average(xs)
+        avgy = average(ys)
+        avg = avgx - avgy
 
         # If the number of samples is large in both samples
         if n1 > 29 and n2 > 29:
@@ -1003,8 +1006,8 @@ class ConfidenceIntervalTwo(Filter):
             ndf = numerator / denumerator
             d = t.ppf(1 - self.sig_level / 2, round(ndf, 0))
 
-        c1 = math.avg - d * sx
-        c2 = math.avg + d * sx
+        c1 = avg - d * sx
+        c2 = avg + d * sx
         self.out['interval'] = (c1, c2)
 
 
