59be650f451ff80361fe5056dcdd15669c2c6b1e
Pascal Wittmann <mail@pascal-wittmann.de>
2012-03-20 15:19:53 +0100

    tests: added tests and test against expected data

--- penchy/tests/test_filter.py
+++ penchy/tests/test_filter.py
@@ -748,13 +748,59 @@ class ComposerTest(unittest.TestCase):
 
 
 class ExportTest(unittest.TestCase):
-    def test_simle(self):
+    def test_simple(self):
         f = Export('/tmp/export', ['test1', 'test2', 'values'],
                    [['v1', 'v2'].__getitem__, ['z1', 'z2'].__getitem__])
         f._run(values=[[1, 2], [3, 4]])
+        expected = "test1\ttest2\tvalues\r\n" \
+            "v1\tz1\t1\r\n" \
+            "v1\tz2\t2\r\n" \
+            "v2\tz1\t3\r\n" \
+            "v2\tz2\t4\r\n"
+        actual = open('/tmp/export').read()
+        try:
+            self.assertMultiLineEqual(actual, expected)
+        finally:
+            os.remove('/tmp/export')
+
+    def test_function_for_value(self):
+        f = Export('/tmp/export', ['test1', 'test2', 'values'],
+                   [['v1', 'v2'].__getitem__, ['z1', 'z2'].__getitem__,
+                    lambda x: "small" if x < 2 else "big"])
+        f._run(values=[[1, 2], [3, 4]])
+        expected = "test1\ttest2\tvalues\r\n" \
+            "v1\tz1\tsmall\r\n" \
+            "v1\tz2\tbig\r\n" \
+            "v2\tz1\tbig\r\n" \
+            "v2\tz2\tbig\r\n"
+        actual = open('/tmp/export').read()
+        try:
+            self.assertMultiLineEqual(actual, expected)
+        finally:
+            os.remove('/tmp/export')
+
+    def test_without_functions(self):
+        f = Export('/tmp/export', ['test1', 'test2', 'values'])
+        f._run(values=[[1, 2], [3, 4]])
+        expected = "test1\ttest2\tvalues\r\n" \
+            "0\t0\t1\r\n" \
+            "0\t1\t2\r\n" \
+            "1\t0\t3\r\n" \
+            "1\t1\t4\r\n"
+        actual = open('/tmp/export').read()
+        try:
+            self.assertMultiLineEqual(actual, expected)
+        finally:
+            os.remove('/tmp/export')
 
     def test_unbalanced_values(self):
         f = Export('/tmp/export', ['test1', 'test2', 'values'],
                    [['v1', 'v2'].__getitem__, ['z1', 'z2'].__getitem__])
         with self.assertRaises(ValueError):
             f._run(values=[[1, [2]], [3, 4]])
+
+    def test_too_depth_values(self):
+        f = Export('/tmp/export', ['test', 'test2', 'values'],
+                   [['v1', 'v2'].__getitem__, ['z1', 'z2'].__getitem__])
+        with self.assertRaises(ValueError):
+            f._run(values=[[[1, 2], [2, 3]], [[3, 4], [4, 5]]])
